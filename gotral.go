50b7316b379789933537bb2d0c10f29f05ef91bf
codenoid <14269809+codenoid@users.noreply.github.com>
2019-10-23 23:35:43 +0700

    do not panic, return error

--- gotral.go
+++ gotral.go
@@ -29,19 +29,19 @@ func createHash(key string) string {
 // Encrypt : encrypt given data with passphrase
 // Load your secret key from a safe place and reuse it across multiple
 // Seal/Open calls.
-func Encrypt(data []byte, passphrase string) []byte {
+func Encrypt(data []byte, passphrase string) ([]byte, error) {
 	// create aes.NewCipher from hashed md5 passphrase
 	block, _ := aes.NewCipher([]byte(createHash(passphrase)))
 	//  NewGCM returns the given 128-bit, block cipher wrapped in
 	// Galois Counter Mode with the standard nonce length.
 	gcm, err := cipher.NewGCM(block)
 	if err != nil {
-		panic(err.Error())
+		return nil, err
 	}
 	// initialize slice with length of nonce that must be passed to Seal and Open.
 	nonce := make([]byte, gcm.NonceSize())
 	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
-		panic(err.Error())
+		return nil, err
 	}
 
 	// Seal encrypts and authenticates plaintext, authenticates the
@@ -49,29 +49,29 @@ func Encrypt(data []byte, passphrase string) []byte {
 	// slice. The nonce must be NonceSize() bytes long and unique for all
 	// time, for a given key.
 	ciphertext := gcm.Seal(nonce, nonce, data, nil)
-	return ciphertext
+	return ciphertext, nil
 }
 
 // Decrypt : decrypt given data with passphrase
 // Load your secret key from a safe place and reuse it across multiple
 // Seal/Open calls.
-func Decrypt(data []byte, passphrase string) []byte {
+func Decrypt(data []byte, passphrase string) ([]byte, error) {
 	// create md5 byte slice
 	key := []byte(createHash(passphrase))
 	// just `reverse` algoritm with passphrase until return
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		panic(err.Error())
+		return nil, err
 	}
 	gcm, err := cipher.NewGCM(block)
 	if err != nil {
-		panic(err.Error())
+		return nil, err
 	}
 	nonceSize := gcm.NonceSize()
 	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
 	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
 	if err != nil {
-		panic(err.Error())
+		return nil, err
 	}
-	return plaintext
+	return plaintext, nil
 }
