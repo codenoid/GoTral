37110a63d56520f944af3ccb2f8c924530c3124e
codenoid <14269809+codenoid@users.noreply.github.com>
2019-10-24 00:46:36 +0700

    [BREAKING CHANGE] new LoadConfig, the old moved to DirectLoad

--- http.go
+++ http.go
@@ -13,6 +13,15 @@ import (
 	"net/http"
 )
 
+// GoTral : enabling uses of basic auth
+type GoTral struct {
+	Url string
+	Passphrase string
+	BasicAuth bool
+	Username string
+	Password string
+}
+
 // config : a data structure that come from GoTral server
 // usually just {"key": "what", "value": "somethingsecret"}
 type config struct {
@@ -24,9 +33,9 @@ type config struct {
 // easy to use/get/understand
 type confret map[string]string
 
-// LoadConfig : load config from given url and decrypt with given
-// passphrase, usually this function only called on app boot time
-func LoadConfig(url string, passphrase string) (confret, error) {
+// DirectLoad : directly load config from given url and decrypt with given passphrase
+// usually this function only called on app boot time
+func DirectLoad(url string, passphrase string) (confret, error) {
 	resp, err := http.Get(url)
 	if err != nil {
 		return nil, err
@@ -39,6 +48,72 @@ func LoadConfig(url string, passphrase string) (confret, error) {
 	}
 
 	decrypt, err := Decrypt(body, passphrase)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decrypt data, got : %v", err.Error())
+	}
+
+	// initialize slices of config to receive data from api
+	var decoded []config
+	err = json.Unmarshal(decrypt, &decoded)
+	if err != nil {
+		return nil, err
+	}
+
+	// initialize empty map[string]string
+	result := make(map[string]string)
+
+	// iterate and put all data into result
+	for _, value := range decoded {
+		// break and return error if there is duplicate key
+		// that come from api
+		if _, ok := result[value.Key]; ok {
+			return nil, fmt.Errorf("error: duplicate config key : %v", value.Key)
+		}
+		result[value.Key] = value.Value
+	}
+
+	return result, nil
+}
+
+// LoadConfig : basic auth version support
+func (r GoTral) LoadConfig() (confret, error) {
+
+	if r.BasicAuth == false {
+		val, err := DirectLoad(r.Url, r.Passphrase)
+		if err != nil { 
+			return nil, err 
+		}
+		return val, nil
+	}
+
+	// initialize http client with/out option
+    client := &http.Client{}
+
+    // create new request by given url
+    req, err := http.NewRequest("GET", r.Url, nil)
+    if err != nil{
+        return nil, err
+    }
+
+    // pass auth for BasicAuth
+    req.SetBasicAuth(r.Username, r.Password)
+
+    resp, err := client.Do(req)
+    if err != nil{
+        return nil, err
+    }
+
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/401
+    if resp.StatusCode == 401 {
+    	return nil, fmt.Errorf("Unauthorized, wrong username/password")
+    }
+
+    body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	decrypt, err := Decrypt(body, r.Passphrase)
 	if err != nil {
 		return nil, fmt.Errorf("failed to decrypt data, got : %v", err.Error())
 	}
